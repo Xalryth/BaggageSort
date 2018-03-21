a60e06911bcb6ce783dbb6806ca3960f2b59fcb8
Xalryth <30471411+Xalryth@users.noreply.github.com>
2018-03-21 09:54:25 +0100

    h
    h

---
+++ BaggageSort/Types/Destination.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaggageSort.Types
+{
+    public enum Destination
+    {
+        Copenhagen,
+        Berlin,
+        London,
+        Dubai,
+        HongKong,
+        NewYork,
+        Bornholm
+    }
+}
---
+++ BaggageSort/Types/Luggage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaggageSort.Types
+{
+    public struct Luggage
+    {
+        UInt32 id;
+        DateTime date;
+        Destination destination;
+
+        public Luggage(uint id, DateTime date, Destination destination)
+        {
+            this.id = id;
+            this.date = date;
+            this.destination = destination;
+        }
+
+
+        public override string ToString()
+        {
+            return $"[{id}]: {destination.ToString()}, {date.ToString("dd-MM-yy")}";
+        }
+    }
+}
