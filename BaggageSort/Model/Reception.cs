30df86dfef5306f64079795d30efd60b021e7368
Xalryth <30471411+Xalryth@users.noreply.github.com>
2018-03-22 08:33:36 +0100

    e
    e

--- BaggageSort/Model/Reception.cs
+++ BaggageSort/Model/Reception.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BaggageSort.Types;
 
 namespace BaggageSort.Model
 {
     class Reception
     {
+        uint count = 0;
+        uint dateStep = 0;
+        Random rnd = new Random();
+
+        public Reception() { }
+
+        public Luggage GenerateLuggage() {
+            count++;
+            dateStep++;
+            return new Luggage(count, new DateTime(0).AddMinutes(dateStep), (Destination)rnd.Next(0, Enum.GetNames(typeof(Destination)).Length - 1));
+        }
     }
 }
