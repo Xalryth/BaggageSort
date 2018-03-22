bef3043c7ff1d3a1caa697fa27dd8e87a681f7ed
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-03-22 08:57:45 +0100

    Terminal class updated

--- BaggageSort/Model/Terminal.cs
+++ BaggageSort/Model/Terminal.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BaggageSort.Types;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,31 @@ namespace BaggageSort.Model
 {
     class Terminal
     {
+        private Queue<Luggage> luggages;
+        private DateTime leaveDate;
+        private Enum destination;
+        private bool locked;
+
+        public Queue<Luggage> Luggages { get => luggages; set => luggages = value; }
+        public DateTime LeaveDate { get => leaveDate; set => leaveDate = value; }
+        public Enum Destination { get => destination; set => destination = value; }
+
+        public Terminal(Enum _dest)
+        {
+            destination = _dest;
+            // leavedate?
+        }
+        // Handle luggage ?
+        public void Fly()
+        {
+            Debug.WriteLine("Plane id flying away");
+            locked = true;
+        }
+        public void SwitchState()
+        {
+            Debug.WriteLine("User locked the terminal");
+            locked = true;
+        }
 
     }
 }
