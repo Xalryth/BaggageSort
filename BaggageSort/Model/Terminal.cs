dfe8714833c0e3c40d0e2b86731cf589206c4c8f
Xalryth <30471411+Xalryth@users.noreply.github.com>
2018-03-22 09:47:34 +0100

    yh

--- BaggageSort/Model/Terminal.cs
+++ BaggageSort/Model/Terminal.cs
@@ -11,23 +11,41 @@ namespace BaggageSort.Model
     public class Terminal
     {
         private Queue<Luggage> luggages;
+        private Queue<Luggage> leftOverLuggage;
         private DateTime leaveDate;
         private Enum destination;
         private bool locked;
+        private int cargoSize = 200;
 
-        public Queue<Luggage> Luggages { get => luggages; set => luggages = value; }
+        public Queue<Luggage> Luggages { get => luggages; private set => luggages = value; }
         public DateTime LeaveDate { get => leaveDate; set => leaveDate = value; }
-        public Enum Destination { get => destination; set => destination = value; }
+        public Enum Destination { get => destination; private set => destination = value; }
+        public Queue<Luggage> LeftOverLuggage { get => leftOverLuggage; private set => leftOverLuggage = value; }
+        public int CargoSize { get => cargoSize; private set => cargoSize = value; }
 
         public Terminal(Enum _dest)
         {
             destination = _dest;
             // leavedate?
         }
-        // Handle luggage ?
+        /// <summary>
+        /// Handles luggage from sorting
+        /// </summary>
+        /// <param name="_luggage"></param>
+        public void HandleLuggage(Luggage _luggage)
+        {
+            if (!locked && luggages.Count() < CargoSize)
+                luggages.Enqueue(_luggage);
+            else
+                leftOverLuggage.Enqueue(_luggage);
+        }
+        public void FillFlight()
+        {
+            luggages.Clear();
+        }
         public void Fly()
         {
-            Debug.WriteLine("Plane id flying away");
+            Debug.WriteLine($"Plane to {Destination.ToString()} is flying away");
             locked = true;
         }
         public void SwitchState()
